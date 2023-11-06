1d25664c9948515376e6a4cdb1f0c9ca23690ad0
nuryy <yanis.nury@etu.umontpellier.fr>
2023-11-06 09:30:52 +0100

    CommandeRemplacer implémenté

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,24 @@
+package fr.iut.editeur.commande;
+
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 2) {
+            System.err.println("Format attendu : remplacer;int depart;int arrive;texte");
+            return;
+        }
+        String texte = parameters[1];
+        this.document.remplacer(Integer.parseInt(parameters[1]),Integer.parseInt(parameters[2]),texte);
+        super.executer();
+    }
+
+}
