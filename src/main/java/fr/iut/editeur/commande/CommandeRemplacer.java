d18f0bc1b5700400b5647ea441b03876f8257b50
nuryy <yanis.nury@etu.umontpellier.fr>
2023-11-06 09:43:52 +0100

    bug fixé, tout marche

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -12,11 +12,11 @@ public class CommandeRemplacer extends CommandeDocument {
 
     @Override
     public void executer() {
-        if(parameters.length < 2) {
+        if(parameters.length < 3) {
             System.err.println("Format attendu : remplacer;int depart;int arrive;texte");
             return;
         }
-        String texte = parameters[1];
+        String texte = parameters[3];
         this.document.remplacer(Integer.parseInt(parameters[1]),Integer.parseInt(parameters[2]),texte);
         super.executer();
     }
