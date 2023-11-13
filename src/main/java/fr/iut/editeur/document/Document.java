a6afc56117387e220da7778635947e698aaeb86f
nuryy <yanis.nury@etu.umontpellier.fr>
2023-11-13 12:07:40 +0100

    bugfix: bug du clear réglé Closes #1

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -24,26 +24,44 @@ public class Document {
     public String toString() {
         return this.texte;
     }
+
+    /**remplace le texte du teste entre deux indices par un autre texte
+     * @param start indice de début de remplacement
+     * @param end indice de fin de remplacement
+     * @param remplacement texte de remplacement
+     * */
     public void remplacer(int start, int end, String remplacement) {
         String leftPart = texte.substring(0, start);
         String rightPart = texte.substring(end);
         texte = leftPart + remplacement + rightPart;
     }
+    /**met le texte en majuscule entre les indice
+    * @param start et
+     * @param end */
     public void majuscules(int start, int end) {
         String leftPart = texte.substring(0, start);
         String middleUppercase=texte.substring(start,end).toUpperCase();
         String rightPart = texte.substring(end);
         texte = leftPart + middleUppercase + rightPart;
     }
+    /** supprime du texte entre les indices
+     * @param start et
+     * @param end */
     public void supprimer(int start, int end){
         String leftPart=texte.substring(0,start);
         String rightPart=texte.substring(end);
         texte=leftPart+rightPart;
     }
+
+    /**supprime l'entièreté du texte*/
     public void clear(){
         texte="";
     }
 
+    /**
+     * insere du texte a un indice donné
+     * @param i indice d'insertion
+     * @param t texte a inserer*/
     public void inserer(int i , String t) {
         String leftPart = texte.substring(0, i);
         String rightPart = texte.substring(i);
